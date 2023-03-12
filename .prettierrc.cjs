a3a0ccd346584679ee5f3cccd71a9deb111237b0
PlutoyDev <99151858+PlutoyDev@users.noreply.github.com>
2023-03-12 14:50:16 +0800

    remove import sort

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -5,13 +5,5 @@ module.exports = {
   singleQuote: true,
   jsxSingleQuote: true,
   trailingComma: 'all',
-  quoteProps: 'consistent',
-  plugins: ['@trivago/prettier-plugin-sort-imports'],
-  importOrder: [
-    '^react',
-    '<THIRD_PARTY_MODULES>',
-    '^[./](?!.*\\.(css|scss|less|styl|png|jpg|jpeg|gif|svg|ttf|woff|woff2|eot|otf)$)',
-    '^[./]',
-  ],
-  importOrderSort: 'asc',
+  quoteProps: 'consistent'
 };
